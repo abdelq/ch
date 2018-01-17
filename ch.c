de9d226a59834b1f549f33177ab8751149aa4c03
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-01-16 21:57:53 -0500

    Cleaning up basic C file

--- ch.c
+++ ch.c
@@ -1,19 +1,18 @@
-/* ch.c.
-auteur:
-date:
-problèmes connus:
-
-  */
+/*
+ * Auteurs : Abdelhakim Qbaich, Rémi Langevin
+ * Date : 2018-01-16
+ * Problèmes connus : S.O.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (void)
+int main(void)
 {
-  fprintf (stdout, "%% ");
+	fprintf(stdout, "%% ");
 
-  /* ¡REMPLIR-ICI! : Lire les commandes de l'utilisateur et les exécuter. */
+	/* TODO : Lire les commandes de l'utilisateur et les exécuter. */
 
-  fprintf (stdout, "Bye!\n");
-  exit (0);
+	fprintf(stdout, "Bye!\n");
+	exit(0);
 }
