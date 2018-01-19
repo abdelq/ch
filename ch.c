ed53ff86b0789cd60bac6004813fb8d68b11f8a7
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-01-18 22:05:24 -0500

    Basic shell

--- ch.c
+++ ch.c
@@ -1,18 +1,64 @@
 /*
  * Auteurs : Abdelhakim Qbaich, Rémi Langevin
- * Date : 2018-01-16
+ * Date : 2018-01-18
  * Problèmes connus : S.O.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#define MAX_ARGS 128		// XXX
+
+// TODO Rename function
+// TODO Manage quotation marks and escape sequences
+// XXX What if we pass MAX_ARGS
+void parse(char **line, char **args)
+{
+	char *arg;
+	while (arg = strsep(line, " ")) {	// XXX " \t\n\r"
+		if (*arg) {
+			*args++ = arg;
+		}
+	}
+	*args = NULL;
+}
 
 int main(void)
 {
-	fprintf(stdout, "%% ");
+	char *line;
+	char *args[MAX_ARGS];
+	pid_t pid;
+	int status;
 
-	/* TODO : Lire les commandes de l'utilisateur et les exécuter. */
+	while (line = readline("$ ")) {
+		if (!*line) {
+			continue;
+		}
 
-	fprintf(stdout, "Bye!\n");
-	exit(0);
+		add_history(line);
+		parse(&line, args);
+
+		// TODO Test
+		if ((pid = fork()) < 0) {
+			perror("ch");
+			return EXIT_FAILURE;
+		}
+
+		if (pid == 0) {
+			if (*args) {
+				execvp(*args, args);	// TODO Read doc about execvp
+				perror("ch");	// TODO Check output w/ perror(*args)
+				// return EXIT_FAILURE ?
+			}
+			return EXIT_SUCCESS;	// XXX
+		} else {
+			waitpid(pid, &status, WUNTRACED);	// TODO Read the doc
+		}
+	}
 }
